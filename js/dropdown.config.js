fae6b6d4ac9307b9702e8a1c2811b5bea8d20df3
Phoenix <117663444@qq.com>
2017-01-28 19:22:10 +0800

    @improve：04#用 Drupal.behaviors 在 Ajax 完成以后调用 JavaScript 方法：解决

--- js/dropdown.config.js
+++ js/dropdown.config.js
@@ -1,3 +1,7 @@
-(function($) {
-  $('.ui.dropdown').dropdown()
-})(jQuery);
+(function($, Drupal) {
+  Drupal.behaviors.dropdown = {
+    attach: function (context, settings) {
+      $(context).find('.ui.search').dropdown()
+    }
+  }
+})(jQuery, Drupal);
